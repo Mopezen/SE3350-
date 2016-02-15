84040736e5e1d997e458201008584c07e0a5e5ec
MeshachJ <Mjone33@uwo.ca>
2016-02-14 19:46:05 -0600

    Added the changes to edit for academic load
    Added the functionality of academic load to the edit student segment

--- FEnd/courseBlog/app/components/student-detail.js
+++ FEnd/courseBlog/app/components/student-detail.js
@@ -5,8 +5,12 @@ export default Ember.Component.extend({
   store: Ember.inject.service(),
   routing: Ember.inject.service('-routing'),
   genderID: null,
+  academicLoadID: null,
   genderModel : Ember.computed('isEditing', function(){
       return this.get('store').findAll('gender');
+  }),
+  academicLoadModel: Ember.computed('isEditing', function(){
+      return this.get('store').findAll('academic-load');
   }),
   isEditing: false,
   actions: {
@@ -19,19 +23,26 @@ export default Ember.Component.extend({
           self.set ('genderID' , res.get('id'));
         }
       });
+      updatedStudent.get('studyLoad').then(function(res){
+        if (res) {
+          self.set ('academicLoadID' , res.get('id'));
+        }
+      });
     },  
     save: function(id){
       this.set('isEditing', false);
       var myStore = this.get('store');
       var genderSelected = myStore.peekRecord('gender', this.$('#gender')[0].value);
+      var academicLoadSelected = myStore.peekRecord('academic-load', this.$('#academicLoad')[0].value);
       var self = this;
       myStore.findRecord('student',id).then(function(student) {
         student.set('number',self.get('selectedStudent.number'));
         student.set('DOB', self.get('selectedStudent.DOB'));
         student.set('gender', genderSelected);
+        student.set('studyLoad',academicLoadSelected);
         student.save();  // => PATCH to /posts/:post_id
       });
-      genderSelected.get('students').pushObject(myStore.findRecord('student',id));
+      //genderSelected.get('students').pushObject(myStore.findRecord('student',id));
       this.set('isEditing', false);
       this.get('routing').transitionTo('students');
     },
